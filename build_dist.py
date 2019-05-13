42cfcd2bb36976f5be295521209155cb1da6087f
Sacharias Sjöqvist <sacharias.sjoqvist@gmail.com>
2019-05-13 14:56:07 +0200

    make dist

--- build_dist.py
+++ build_dist.py
@@ -11,19 +11,31 @@ from torch.utils import data as D
 
 from customdataset import TinyImageNet, imshow_combine
 
+dist = np.zeros((22,22))
+
 path = 'data/canary-1/'
 tiny_imagenet = TinyImageNet(path, build_dist=True)
 
+stop = 0
+
 for i, data in enumerate(D.DataLoader(tiny_imagenet, batch_size=1, shuffle=False), 0):
+    stop += 1
+    if stop > 5:
+        break
     a_channel = data[0, 0, :, :].numpy()
     b_channel = data[0, 1, :, :].numpy()
     X, Y = a_channel.shape
-    print('start', X, Y)
+    print('i', i)
     for x in range(X):
         for y in range(Y):
-            print(x, y)
+            a = a_channel[x, y]
+            a = np.floor_divide(np.round(a + 110), 10)
+            a = int(a)
 
+            b = b_channel[x, y]
+            b = np.floor_divide(np.round(b + 110), 10)
+            b = int(b)
 
-# inputs, labels = dataiter.next()
-# print(inputs.size())
-# print(labels.size())
+            dist[a,b] = dist[a,b] + 1
+
+print(np.where(dist > 0))
